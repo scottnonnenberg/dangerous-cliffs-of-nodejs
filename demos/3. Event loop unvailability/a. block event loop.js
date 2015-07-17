aec50881e332953ac859475e7e01d494fa5ebdea
Scott Nonnenberg <scott@nonnenberg.com>
2015-07-17 15:01:01 -0700

    Update Event loop availability demos

---
+++ demos/3. Event loop unvailability/a. block event loop.js
@@ -0,0 +1,52 @@
+
+'use strict';
+
+var fs = require('fs');
+var toobusy = require('toobusy-js');
+
+
+var doSyncWork = function() {
+  var start = new Date();
+  var now = new Date();
+
+  console.log('doSyncWork: start');
+  while (now.getTime() - start.getTime() < 1000) {
+    now = new Date();
+  }
+  console.log('doSyncWork: done');
+};
+
+var getFile = function() {
+  var start = new Date();
+  console.log('getFile: start');
+  fs.readFile('nonexistent', function() {
+    var now = new Date();
+    var delta = now.getTime() - start.getTime()
+    console.log('getFile: done', delta + 'ms');
+  });
+};
+
+var previous;
+var writeInterval = function() {
+  var delta;
+  var now = new Date();
+  if (previous) {
+    delta = now.getTime() - previous.getTime();
+  }
+  console.log('writeInterval:', delta ? delta + 'ms' : '-');
+  previous = now;
+};
+
+
+setInterval(writeInterval, 100);
+
+setTimeout(getFile, 250)
+
+setTimeout(function() {
+  getFile();
+  doSyncWork();
+}, 500)
+
+setTimeout(function() {
+  process.exit();
+}, 2000);
--- demos/3. Event loop unvailability/b. choke event loop.js
+++ demos/3. Event loop unvailability/b. choke event loop.js
@@ -3,31 +3,42 @@
 
 var async = require('async');
 var toobusy = require('toobusy-js');
-var _ = require('lodash');
 
-// all these in milliseconds
-var LAUNCH_DELAY = 1;
-var EXPANSION = 3;
+// all in milliseconds
+
+var LAUNCH_DELAY = 10;
+  // start: 10 = 100 requests/second
+  // try: 5 = 200 requests/second
+var EXPANSION = 1;
 var TASK_DELAY = 20;
-var SYNC_WORK = 40;
+var SYNC_WORK = 10;
 
-var REJECT_IF_TOOBUSY = true;
+var REJECT_IF_TOOBUSY = false;
+  // then: turn this on, see if it helps
 
 var concurrent = 0;
 var completed = 0;
 
-var doSync = function() {
+
+var doSyncWork = function() {
   var start = new Date();
   var now = new Date();
 
   while (now.getTime() - start.getTime() < SYNC_WORK) {
     now = new Date();
   }
+};
 
-  return _.random(0, 10);
-}
+var doTask = function(cb) {
+  concurrent += 1;
+  setTimeout(function() {
+    concurrent -= 1;
+    completed += 1;
+    return cb(null, doSyncWork());
+  }, TASK_DELAY);
+};
 
-var handle = function() {
+var launchTasks = function() {
   var tasks = [];
 
   if (REJECT_IF_TOOBUSY && toobusy()) {
@@ -35,14 +46,7 @@ var handle = function() {
   }
 
   for (var i = 0; i < EXPANSION; i += 1) {
-    tasks.push(function(cb) {
-      concurrent += 1;
-      setTimeout(function() {
-        concurrent -= 1;
-        completed += 1;
-        return cb(null, doSync());
-      }, TASK_DELAY);
-    });
+    tasks.push(doTask);
   }
 
   async.parallel(tasks, function(err, result) {
@@ -50,17 +54,35 @@ var handle = function() {
   });
 };
 
-setInterval(function() {
+var writeStatus = function() {
   console.log('lag:', toobusy.lag());
   console.log('concurrent:', concurrent);
   console.log('completed:', completed);
   completed = 0;
-}, 250);
+};
 
+
+setInterval(writeStatus, 250);
+
+var previous;
 var go = function() {
   setTimeout(function() {
-    handle();
+    var now = new Date();
+    var count = 1;
+    if (previous) {
+      // we're limited by event loop, so here we replicate lots of
+      // events coming in while event loop blocked
+      var delta = now.getTime() - previous.getTime();
+      count = Math.floor(delta / LAUNCH_DELAY);
+      // console.log('catch-up multipler:', count)
+    }
+    for (var i = 0; i < count; i+= 1) {
+      launchTasks();
+    }
+    previous = now;
+
     go();
+
   }, LAUNCH_DELAY)
 };
 
