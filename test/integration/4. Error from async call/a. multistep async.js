c1017edebdab887091e540e5caa981f4a435afb5
Scott Nonnenberg <scott@nonnenberg.com>
2015-07-17 16:08:13 -0700

    Fix/improve tests for  'Error from async callback'

--- test/integration/4. Error from async call/a. multistep async.js
+++ test/integration/4. Error from async call/a. multistep async.js
@@ -6,19 +6,21 @@ var path = require('path');
 var test = require('thehelp-test');
 var expect = test.expect;
 var supertest = require('supertest');
+var core = require('thehelp-core');
 
 var startProcess = require('../start_process');
 
-var multistep = require('../../../demos/5. Error from async call/a. multistep async');
+var multistep = require('../../../demos/4. Error from async call/a. multistep async');
 
-describe('5. Error from async call, a. multistep async', function() {
+describe('4. Error from async call, a. multistep async', function() {
 
   it('returns an error', function(done) {
     multistep(function(err, result) {
 
       expect(err).to.have.property('stack');
 
-      console.log(err.stack);
+      console.log(core.breadcrumbs.toString(err));
+
       expect(err.stack.split('\n')).to.have.length(3);
 
       done();
--- test/integration/4. Error from async call/b. with breadcrumbs.js
+++ test/integration/4. Error from async call/b. with breadcrumbs.js
@@ -6,22 +6,27 @@ var path = require('path');
 var test = require('thehelp-test');
 var expect = test.expect;
 var supertest = require('supertest');
+var core = require('thehelp-core');
 
 var startProcess = require('../start_process');
 
 var withBreadcrumbs =
-  require('../../../demos/5. Error from async call/b. with breadcrumbs');
+  require('../../../demos/4. Error from async call/b. with breadcrumbs');
 
 
-describe('5. Error from async call, a. with breadcrumbs', function() {
+describe('4. Error from async call, a. with breadcrumbs', function() {
 
   it('error includes the step function name', function(done) {
     withBreadcrumbs(function(err, result) {
 
       expect(err).to.have.property('stack');
 
-      console.log(err.stack);
+      console.log(core.breadcrumbs.toString(err));
+
       expect(err.stack.split('\n')).to.have.length(5);
+
+      expect(err).to.have.property('userId').that.exist;
+      expect(err).to.have.property('command').that.exist;
 
       done();
     });
