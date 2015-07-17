795ff24eddb70ee78ac8b431bc95aea0b9b26ca2
Scott Nonnenberg <scott@nonnenberg.com>
2015-07-17 16:34:07 -0700

    Implement Hangs/Hapi test - no logging for hangs!

--- demos/2. Hangs/c. hapi and good-console, no callback.js
+++ demos/2. Hangs/c. hapi and good-console, no callback.js
@@ -1,1 +1,53 @@
-c. hapi, no callback.js
+
+'use strict';
+
+var fs = require('fs');
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({
+  port: 3000
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply('<html><body>'
+      + '<div><a href="/hang">Will not return</a></div>'
+      + '</body></html>');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/hang',
+  handler: function(request, reply) {
+    // no callback!
+  }
+});
+
+var options = {
+  opsInterval: 1000,
+  reporters: [{
+    reporter: require('good-console'),
+    events: {
+      log: '*',
+      response: '*'
+    }
+  }]
+};
+
+server.register({
+  register: require('good'),
+  options: options
+}, function (err) {
+  if (err) {
+    console.error(err.stack);
+  }
+  else {
+    server.start(function () {
+      console.info('Server started at ' + server.info.uri);
+    });
+  }
+});
--- test/integration/2. Hangs/c. hapi and good-console, no callback.js
+++ test/integration/2. Hangs/c. hapi and good-console, no callback.js
@@ -10,5 +10,49 @@ var supertest = require('supertest');
 var startProcess = require('../start_process');
 
 describe('2. Hangs, c. hapi and good-console, no callback', function() {
+  var child, agent;
 
+  before(function(done) {
+    agent = supertest.agent('http://localhost:3000');
+
+    var entrypoint = path.join(__dirname,
+      '../../../demos/2. Hangs/c. hapi and good-console, no callback.js');
+
+    child = startProcess(entrypoint);
+
+    setTimeout(done, 1000);
+  });
+
+  it('returns 200 for root', function(done) {
+    agent
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('request to /hang never returns', function(done) {
+    var timeout = 4000;
+
+    this.timeout(timeout + 2000);
+
+    agent
+      .get('/hang')
+      .timeout(timeout)
+      .end(function(err, res) {
+        /* jshint unused: false */
+        expect(err).to.have.property('timeout', timeout);
+        setTimeout(done, 100);
+      });
+  });
+
+  it('shuts down', function(done) {
+    child.kill();
+
+    child.on('close', function() {
+      expect(child).to.have.property('result');
+
+      expect(child.result.split('\n')).to.have.length(3);
+
+      done();
+    });
+  });
 });
